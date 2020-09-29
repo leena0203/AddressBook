74f65688823524bc7b7a4f748216c01ab34c58b4
leena0203 <leenasarode02@gmail.com>
2020-09-29 20:45:06 +0530

    ContactClassUC1 committed

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,10 @@
 package address;
 
+import java.util.Scanner;
+
 public class AddressBook {
      public static void main(String[] args) {
+    	 Scanner sc = new Scanner(System.in);
     	 System.out.println("Welcome to Address Book Program");
      }
 }
